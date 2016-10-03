09241e870ce75374817239a190925117ca13d1aa
Anandakumar Palanisamy <ananda11k@gmail.com>
2016-10-04 00:49:32 +0530

    Setup gulp task for sass with compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var jsSources = ['components/scripts/rclick.js',
                 'components/scripts/pixgrid.js',
                 'components/scripts/tagline.js',
                 'components/scripts/template.js'];
+
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function(){
     gulp.src('components/coffee/tagline.coffee')
@@ -23,4 +26,16 @@ gulp.task('js', function(){
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+});
+
+
+gulp.task('compass', function(){
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'build/development/images',
+            style: 'expanded'
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('build/development/css'))
 });
